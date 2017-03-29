003a2cdf0a64a985d2e27ba6b05e26bd26c355da
dposs <daniel.possamai.cadastros@live.com>
2017-03-29 16:50:14 -0300

    1.3.2

--- src/com/faminto/dao/VotacaoDao.java
+++ src/com/faminto/dao/VotacaoDao.java
@@ -8,9 +8,6 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-import javax.faces.bean.ApplicationScoped;
-import javax.faces.bean.ManagedBean;
-
 import com.faminto.model.Votacao;
 
 /**
